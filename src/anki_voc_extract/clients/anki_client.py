9c96d35cae56fbaf58bf872190c9303b464306a3
lin-pei-hsuan <lin-pei-hsuan@dmm.com>
2025-02-15 16:30:04 +0900

    Add: get_cards method

--- src/anki_voc_extract/clients/anki_client.py
+++ src/anki_voc_extract/clients/anki_client.py
@@ -1,17 +1,23 @@
+import requests
 from injector import inject
 
-from anki_voc_extract.config import Config
+from anki_voc_extract.configs import AnkiClientConfig
 
 
 class AnkiClient:
     @inject
-    def __init__(self, config: Config) -> None:
+    def __init__(self, config: AnkiClientConfig) -> None:
         self.config = config
 
     def get_api_url(self) -> str:
         """Return a api url.
 
         Returns:
-            _type_: _description_
+            str: API url for AnkiConnector.
         """
         return self.config.api_url
+
+    def get_cards(self):
+        payload = {"action": "notesInfo", "version": 6, "params": {"query": "deck:korean"}}
+
+        return requests.post(self.config.api_url, json=payload, timeout=self.config.timeout)
