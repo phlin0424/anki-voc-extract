0e5a4fcf6d3159589c5b935a65c8a61bdcdb9196
lin-pei-hsuan <lin-pei-hsuan@dmm.com>
2025-05-02 20:33:47 +0900

    Refactor: reorder __all__ exports for consistency and clarity

--- src/anki_voc_extract/utils/__init__.py
+++ src/anki_voc_extract/utils/__init__.py
@@ -1,5 +1,9 @@
+from anki_voc_extract.utils.ai_agent import AIAgent
 from anki_voc_extract.utils.anki_text_cleaner import AnkiTextCleaner
 from anki_voc_extract.utils.outputter import Outputter
-from anki_voc_extract.utils.ai_agent import AIAgent
 
-__all__ = ["AnkiTextCleaner", "Outputter", "AIAgent"]
+__all__ = [
+    "AIAgent",
+    "AnkiTextCleaner",
+    "Outputter",
+]
